79e47e059908b25dce70b9c7f24d7c3874f1486f
Ariez <arzmess@gmail.com>
2023-12-29 12:08:16 +0800

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  { path: '', component: HomeComponent },
+
+  { path: 'about', loadChildren: () => 
+  import('../app/about/about.component').then((m) => m.AboutComponent) }, 
+ 
+{ path: 'contact', loadChildren: () =>
+import('../app/contact/contact.component').then((m) => m.ContactComponent) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <app-navbar></app-navbar>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'wick';
